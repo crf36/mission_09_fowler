7abe7c43005a8eb81bb2aee84a53c6c84b18df20
crf36 <149890484+crf36@users.noreply.github.com>
2024-03-04 21:15:45 -0700

    Completed Mission

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import teamsData from "./CollegeBasketballTeams.json";
+
+interface BasketballTeam {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+class Team extends React.Component<BasketballTeam> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div className="team-box">
+        <h2>{school}</h2>
+        <h3>Mascot: {name}</h3>
+        <p>
+          Location: {city}, {state}
+        </p>
+      </div>
+    );
+  }
+}
+
+function ListTeams() {
+  return (
+    <div className="team-container">
+      {teamsData.teams.map((oneTeam) => (
+        <Team key={oneTeam.tid} {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="header">
+        <h1>March Madness Basketball Season 2024</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          On this website, you will find info about every college team that will
+          be competing in the NCAA this year!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="container">
+        <ListTeams />
+      </div>
+      <div className="footer">
+        <p>© 2024 March Madness</p>
+      </div>
     </div>
   );
 }
